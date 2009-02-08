5c8565382efe65827a2816d7fd771cfc6bd205f8
Eric Holscher <eric@ericholscher.com>
2009-02-08 14:49:50 -0600

    Making a pretty picture with the socialgraph

--- socialgraph/models.py
+++ socialgraph/models.py
@@ -3,61 +3,61 @@
 import simplejson as json
 import urllib2
 from django.db import models
+from django.template.defaultfilters import slugify
+import graph
+import gv
+
 
 query_url = "http://socialgraph.apis.google.com/lookup?q=%s&fme=1&edo=1&edi=1&pretty=1"
 
 class Request(models.Model):
-    url = models.URLField(unique=True, primary_key=True)
-    canonical_mapping = models.URLField(blank=True, null=True, unique=True)
+    url = models.URLField(unique=True)
+    canonical_mapping = models.URLField(blank=True, null=True)
+    slug = models.SlugField()
     json_obj = models.TextField(blank=True)
 
-    nodes_referenced = models.ManyToManyField('Request', related_name='nodes_referenced_by', null=True)
-    claimed_nodes = models.ManyToManyField('Request', related_name='unverified_claiming_nodes', null=True)
-    toplevel_nodes = models.ManyToManyField('Request', related_name='parents', null=True)
+    nodes_referenced = models.ManyToManyField('self', related_name='nodes_referenced_by', null=True)
+    claimed_nodes = models.ManyToManyField('self', related_name='unverified_claiming_nodes', null=True)
+    toplevel_nodes = models.ManyToManyField('self', related_name='parents', null=True)
     #types?
+    populated = models.BooleanField(default=False)
 
-    def fetch_social_object(self, top_level=True):
-        if self.json_obj:
-            return self.json_obj
+    def fetch_social_object(self):
+        #if self.populated:
+            #return self.json_obj
         print "querying %s" % self.url
         resp = urllib2.urlopen(query_url % self.url)
         self.json_obj = resp.read()
         self.save()
         return json.JSONDecoder().decode(self.json_obj)
 
-    def populate_structure(self, fetch=False, recurse=False):
-        if self.canonical_mapping:
-            print "%s already exits" % self.url
+    def populate_structure(self):
+        if self.populated:
+            print "%s already populated" % self.url
             return
         try:
             python_obj = self.fetch_social_object()
         except:
             print "(HTTP) FAIL"
             return
+
         self.canonical_mapping = python_obj['canonical_mapping'].values()[0]
+        self.populated = True
 
         if python_obj:
             for url,node in python_obj['nodes'].items():
                 t, created = Request.objects.get_or_create(url=url)
-                if recurse:
-                    t.populate_structure(recurse=True)
-                elif created:
-                    t.populate_structure()
+                t.populate_structure()
                 self.toplevel_nodes.add(t)
 
                 for claimed_node in node.get('claimed_nodes', []):
                     n, created = Request.objects.get_or_create(url=claimed_node)
-                    if recurse:
-                        n.populate_structure(recurse=True)
                     self.claimed_nodes.add(n)
 
                 for name,types in node.get('nodes_referenced',{}).iteritems():
                     n, created = Request.objects.get_or_create(url=name)
-                    if recurse:
-                        n.populate_structure(recurse=True)
                     n.types = types['types']
                     self.nodes_referenced.add(n)
-
                 """
                 for unverified_node in node.get('unverified_claiming_nodes', []):
                     n, created = Request.objects.get_or_create(url=unverified_node)
@@ -70,17 +70,23 @@ class Request(models.Model):
                 """
         self.save()
 
+    def pretty_picture(self):
+        if not self.populated:
+            self.populate_structure()
 
+        gr = graph.graph()
+        gr.add_nodes([self.url])
+        toplevel = self.toplevel_nodes.all()
+        gr.add_nodes(list(toplevel))
+        for node in toplevel:
+            print "Making %s" % node
+            gr.add_nodes([str(node)])
+            gr.add_edge(self.url, str(node))
 
-    def loves(self, lover):
-        my_nodes = self.urls_claimed
-        lover_nodes = lover.urls_referenced
-        for node in my_nodes:
-            if node in lover_nodes:
-                print "%s loves %s" % (lover, node)
-                return True
-        return False
-
+        dot = gr.write(fmt='dot')
+        gvv = gv.readstring(dot)
+        gv.layout(gvv,'dot')
+        gv.render(gvv,'png','test.png')
 
     def __unicode__(self):
         return self.url
@@ -91,15 +97,11 @@ class Request(models.Model):
     def __repr__(self):
         return "<Request object: %s>" % self.url
 
-
-class Attributes(models.Model):
-    request = models.ForeignKey(Request, related_name='attributes')
-
-    def populate_structure(self):
-        self.url = self.kwargs.get('url', '')
-        self.profile = self.kwargs.get('profile', '')
-        self.rss = self.kwargs.get('rss', '')
-        self.atom = self.kwargs.get('atom', '')
-        self.foaf = self.kwargs.get('foaf', '')
-        self.photo = self.kwargs.get('photo', '')
-        self.fn = self.kwargs.get('fn', '')
+    def save(self, force_insert=False, force_update=False):
+        temp_url = self.url
+        temp_url = temp_url.replace('http://', '')
+        #import ipdb; ipdb.set_trace()
+        temp_url = temp_url.replace('www.', '')
+        temp_url = temp_url.rstrip('/ ')
+        self.slug = slugify(self.url)
+        super(Request, self).save(force_insert, force_update)
--- socialgraph/views.py
+++ socialgraph/views.py
@@ -4,11 +4,12 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from socialgraph.models import Request
 from django.http import HttpResponse, Http404
+from djangopeople.models import Service
 
 
 def socialgraph_json(request, url):
     r, created = Request.objects.get_or_create(url=url)
-    if created:
+    if created or not r.populated:
         r.populate_structure()
     return HttpResponse(r.json_obj, mimetype='application/javascript')
 
@@ -19,6 +20,15 @@ def process_url(request):
         return Http404('Please post, yo')
 
     r, created = Request.objects.get_or_create(url=url)
-    if created:
+    if created or not r.populated:
         r.populate_structure()
-    return HttpResponse(r.json_obj, mimetype='application/javascript')
+    #ret_val = r.json_obj
+    claimed = list(r.claimed_nodes.all())
+    fake_friends = Request.objects.filter(nodes_referenced__in=r.claimed_nodes.all()).distinct()
+    friends = fake_friends.in_bulk([r.id for r in r.nodes_referenced.all()]).values()
+    #friends = r.nodes_referenced.all().distinct()
+    ret_val = "%s = %s" % (len(fake_friends), len(friends))
+    #ret_val = ["friend ->: %s \n" % url for url in friends]
+    #ret_val += ["<- friend: %s \n" % url for url in fake_friends]
+    #ret_val.append("\n\n\n%s" % r.json_obj)
+    return HttpResponse(ret_val, mimetype='application/javascript')
